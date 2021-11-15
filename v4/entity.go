8cbc29806480079b004ba321425aa8d83e02e2af
freer <freer@iso.io>
2021-11-14 22:11:40 -0800

    Introduce Entity interface type.

---
+++ v4/entity.go
@@ -0,0 +1,22 @@
+/* Copyright 2021 Freerware
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package work
+
+// Entity represents an object defined by its identity, not by its attributes.
+type Entity interface {
+	// ID retrieves the identity for the object.
+	ID() interface{}
+}
